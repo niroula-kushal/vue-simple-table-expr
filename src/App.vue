acca8ffee2db86b599e5d9f3039de4fc2a84e7cd
Kushal Niroula <niroulakushal31@gmail.com>
2022-12-06 21:35:00 +0545

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import SimpleTable from './SimpleTable.vue';
+
+const items = ref([
+  { id: 1, name: "Hola", isActive: true },
+  { id: 2, name: "BHola", isActive: false },
+  { id: 3, name: "SHola", isActive: true },
+]);
+  
+const columns = ref([
+  { header: "Id", name:"id"},
+  { header: "Name", name:"name" },
+  { header: "Status", name:"status", displayField: "isActive" },
+  { header: "Action", name:"action" },
+]);
+
+</script>
+
+<template>
+  <SimpleTable :data="items" :column-defs="columns">
+    <template #col.status="{ item }">
+      <span v-if="item.isActive">
+      	<input type="checkbox" checked disabled/>
+      </span>
+      <span v-else>
+      	<input type="checkbox" disabled/>
+      </span>
+    </template>
+  </SimpleTable>
+</template>
---
+++ src/SimpleTable.vue
@@ -0,0 +1,45 @@
+<template>
+    <table>
+        <thead>
+          <tr>
+            <th v-for="column in columnDefs">
+              {{column.header}}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr v-for="item in data">
+            <td v-for="column in columnDefs">
+              <slot :name="`col.${column.name}`" :item="item">
+              <template v-if="column.displayField">
+                  {{item[column.displayField]}}
+              </template>
+              <template v-else="column.displayField">
+                  {{item[column.name]}}
+              </template>
+            </slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </template>
+    
+    <script setup>
+    const props = defineProps({
+      data: Array,
+      columnDefs: Array
+    });
+    </script>
+    
+    <style>
+      table {
+        width: 100%;
+        color: gray;
+        border-collapse: collapse;
+      }
+      td,th {
+        text-align: left;
+        border: 1px solid black;
+        padding: 10px;
+      }
+    </style>
